44b193cc004a8114ae0122fe54d991b08d5cb6a9
KPavlov <astartes@live.ru>
2020-01-31 17:12:51 +1000

    update

--- conf.py
+++ conf.py
@@ -53,16 +53,11 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-from recommonmark.parser import CommonMarkParser
-source_suffix = ['.rst', '.md']
-source_parsers = {
-    '.md': CommonMarkParser,
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
 }
-#source_suffix = {
-#    '.rst': 'restructuredtext',
-#    '.txt': 'markdown',
-#    '.md': 'markdown',
-#}
 
 
 # The master toctree document.
